5505c5229efe5a859a5e61ec0beb1d428604d2e0
Bramdon Santiago <bramdsantiago@gmail.com>
2026-07-29 14:56:51 -0600

    Update CV link

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -45,7 +45,7 @@ function About() {
                 <p>Hi, I am Bramdon, a technology enthusiast from Mexico. I am passionate about web design and development, especially everything related to visual aspects and interactivity. I love working on the frontend. Additionally, I enjoy designing my own sites, so I have experience with design tools such as Figma and Adobe XD. I have been involved from the beginning through to the launch of multiple projects. I'm seeking new challenges. I understand UI/UX, and as part of the challenges I have faced, I have successfully designed and developed my own interfaces and prototypes. I have experience with agile methodologies, such as Scrum.</p>
                 <p>I am currently looking to stay in a job that involves 100% React or Angular projects, although I have a slight preference for Angular, I have no problems working within the ecosystem of other frontend technologies. I am looking to stay and continue working 100% remote.</p>
                 <div className='social-media' style={{ justifyContent: 'center' }}>
-                    <a href='https://www.linkedin.com/in/bramdonsantiago' target='_blank' className='singular-social-media'>
+                    <a href='https://www.linkedin.com/in/bramdonsantiago-frontenddeveloper/' target='_blank' className='singular-social-media'>
                         <i className='fa-brands fa-linkedin-in'></i>
                     </a>
                     <a href='https://wa.me/+524381338561?text=Hola, Bramdon. ¿Cómo estás?, ¿Podríamos tener una entrevista?' target='_blank' className='singular-social-media'>
@@ -56,7 +56,7 @@ function About() {
                     </a>
                 </div>
                 <div className='btn-cv-content'>
-                    <a href='https://drive.google.com/file/d/13m2Iqgu-a62yb-jm0EhMW6uszFblTsHk/view?usp=drive_link' target='_blank' className='btn btn-outline-terteary'>Download CV</a>
+                    <a href='https://drive.google.com/file/d/1Lnp5SOgcBw8XeGUuqxK7Vaf1DkesVqTC/view?usp=sharing' target='_blank' className='btn btn-outline-terteary'>Download CV</a>
                 </div>
             </div>
         </div>
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -28,7 +28,7 @@ function Contact() {
                         </div>
                     </div>
                     <div className='social-media'>
-                        <a href='https://www.linkedin.com/in/bramdonsantiago' target='_blank' className='singular-social-media'>
+                        <a href='https://www.linkedin.com/in/bramdonsantiago-frontenddeveloper/' target='_blank' className='singular-social-media'>
                             <i className='fa-brands fa-linkedin-in'></i>
                         </a>
                         <a href='https://wa.me/+524381338561?text=Hola, Bramdon. ¿Cómo estás?, ¿Podríamos tener una entrevista?' target='_blank' className='singular-social-media'>
